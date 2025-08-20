ada10aa573aa3c2fad25ff7d973452862e823154
harshbhardwaj77 <harsh.harshbhardwaj.bhardwaj@gmail.com>
2025-08-20 16:13:06 +0530

    Added files

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,6 @@
 terraform {
   required_version = ">= 1.6.0"
+
   required_providers {
     google = {
       source  = "hashicorp/google"
@@ -11,6 +12,7 @@ terraform {
 provider "google" {
   project     = var.project_id
   region      = var.region
+  # var.GOOGLE_CREDENTIALS_JSON must be a BASE64 string of your SA JSON
   credentials = base64decode(var.GOOGLE_CREDENTIALS_JSON)
 }
 
@@ -42,8 +44,9 @@ resource "google_compute_instance" "web" {
 
   boot_disk {
     initialize_params {
-      source_image = data.google_compute_image.ubuntu_2204.self_link
-      size         = 30
+      # FIX: use 'image' (not source_image) and point to 24.04
+      image = data.google_compute_image.ubuntu_2404.self_link
+      size  = 30
     }
   }
 
@@ -52,15 +55,15 @@ resource "google_compute_instance" "web" {
     access_config {}
   }
 
-  # Optional SSH access + pass app_type via metadata as well
+  # Optional SSH access; also pass app_type via metadata
   metadata = {
     ssh-keys = "ubuntu:${var.ssh_public_key}"
     app_type = var.app_type
   }
 
-  # Use a template so only ${app_type} is interpolated by Terraform
+  # Use the template in ../scripts relative to infra/
   metadata_startup_script = templatefile(
-    "${path.module}/scripts/setup.sh.tftpl",
+    "${path.module}/../scripts/setup.sh.tftpl",
     { app_type = var.app_type }
   )
 
