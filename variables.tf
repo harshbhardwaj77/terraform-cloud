38d85237c022d593d5c56e40593409530871c23d
harshbhardwaj77 <harsh.harshbhardwaj.bhardwaj@gmail.com>
2025-08-20 10:56:55 +0530

    Added files

--- variables.tf
+++ variables.tf
@@ -1,10 +1,37 @@
-variable "project_id" {}
-variable "region"     {}
-variable "zone"       {}
-variable "credentials_file" {}
-variable "ssh_private_key" {}
-variable "app_type" {
-  description = "Choose app type: wordpress or laravel"
+variable "GOOGLE_CREDENTIALS_JSON" {
+  description = "Base64 encoded GCP credentials JSON"
   type        = string
-  default     = "wordpress"
+  sensitive   = true  # Marked as sensitive for security
+}
+
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "The GCP region"
+  type        = string
+}
+
+variable "zone" {
+  description = "The GCP zone"
+  type        = string
+}
+
+variable "app_type" {
+  description = "Choose the app type: wordpress or laravel"
+  type        = string
+  default     = "wordpress"  # Default value
+}
+
+variable "ssh_private_key" {
+  description = "The private SSH key for accessing the instance"
+  type        = string
+  sensitive   = true  # Marked as sensitive for security
+}
+
+variable "ssh_public_key" {
+  description = "The public SSH key for the instance"
+  type        = string
 }
