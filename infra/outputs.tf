1773fbdf8fd2615809eb26dd78d3bd6b5feef924
harshbhardwaj77 <harsh.harshbhardwaj.bhardwaj@gmail.com>
2025-08-22 15:45:53 +0530

    Added files

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,11 +1,9 @@
 output "bucket_name" {
   description = "Name of the created GCS bucket"
-  value       = module.bucket.bucket_name
-  condition   = var.create_bucket
+  value       = try(module.bucket.bucket_name, null)
 }
 
 output "vm_ip" {
   description = "External IP of the created VM"
-  value       = module.vm.instance_ip
-  condition   = var.create_vm
+  value       = try(module.vm.instance_ip, null)
 }
