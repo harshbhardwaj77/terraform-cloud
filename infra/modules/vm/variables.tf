03b75cfb418cd18ce30a3d91d3628d57bd510873
harshbhardwaj77 <harsh.harshbhardwaj.bhardwaj@gmail.com>
2025-08-22 16:21:54 +0530

    Added files

--- infra/modules/vm/variables.tf
+++ infra/modules/vm/variables.tf
@@ -1,33 +1,19 @@
 variable "instance_name" {
-  description = "Name of the compute instance"
+  description = "Name of the VM instance"
   type        = string
 }
 
-variable "zone" {
-  description = "GCP zone"
+variable "project_id" {
+  description = "GCP Project ID"
   type        = string
 }
 
-variable "machine_type" {
-  description = "Machine type for the instance"
+variable "region" {
+  description = "GCP Region"
   type        = string
-  default     = "e2-medium"
 }
 
-variable "boot_image" {
-  description = "Boot disk image to use"
+variable "GOOGLE_CREDENTIALS_JSON" {
+  description = "Base64 encoded SA credentials JSON"
   type        = string
-  default     = "ubuntu-os-cloud/ubuntu-2004-lts"
-}
-
-variable "network" {
-  description = "Network name to use"
-  type        = string
-  default     = "default"
-}
-
-variable "tags" {
-  description = "List of network tags"
-  type        = list(string)
-  default     = ["cloudpanel"]
 }
