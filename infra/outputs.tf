f451cde4a5c84ddb218626f80d75d8f798f593b7
harshbhardwaj77 <harsh.harshbhardwaj.bhardwaj@gmail.com>
2025-08-22 16:54:59 +0530

    Added files

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,4 +1,4 @@
 output "vm_ip" {
-  description = "External IP of the CloudPanel VM"
+  description = "The external IP address of the VM"
   value       = google_compute_instance.cloudpanel_vm.network_interface[0].access_config[0].nat_ip
 }
